da841ccd5a04059cfaadcf8770d3749315a7f35a
Boris Vasilyev <1024sparrow@gmail.com>
2021-11-30 07:27:28 +0300

    kaz

--- Makefile
+++ Makefile
@@ -6,11 +6,12 @@ PROGRAM_NAME = md5sum
 
 all: $(PROGRAM_NAME) $(PROGRAM_NAME)-win64.exe
 
-$(PROGRAM_NAME)-win64.exe: *.c
-	x86_64-w64-mingw32-gcc $^ -o $@
+$(PROGRAM_NAME)-win64.exe: *.c SRC
+	#x86_64-w64-mingw32-gcc $^ -o $@
+	x86_64-w64-mingw32-gcc *.c -o $@
 
-$(PROGRAM_NAME): *.c
-	$(CC) $(CFLAGS) -o $@ $^
+$(PROGRAM_NAME): *.c SRC
+	$(CC) $(CFLAGS) *.c -o $@
 
 clean:
 	rm $(PROGRAM_NAME) $(PROGRAM_NAME)-win64.exe
--- validate_analizing_path.c
+++ validate_analizing_path.c
@@ -17,5 +17,50 @@ validateAnalizingPath(
         return 0;
     }
 
+    struct descr {
+        enum {
+            FILETYPE_ANY,
+            FILETYPE_FILE,
+            FILETYPE_DIR
+        } fileType;
+        enum {
+            FILTERTYPE_FULLPATH_EQUAL = 1,
+            FILTERTYPE_FULENAME_EQUAL = 2,
+            FILTERTYPE_FULLPATH_STARTS_WITH = 3,
+            FILTERTYPE_FULENAME_STARTS_WITH = 4
+        } filterType;
+        const char * filename;
+    } descr [] = {
+        /*{
+            FILETYPE_DIR,
+            FILTERTYPE_FULLPATH_EQUAL,
+            "./Journal"
+        }*/
+        #include "SRC"
+    };
+    int i;
+    struct descr *oDescr;
+    for (i = 0, oDescr = &descr ; i < sizeof(descr)/sizeof(struct descr) ; ++i, ++oDescr){
+        if (oDescr->fileType != FILETYPE_ANY){
+            puts("NOT IMPLEMENTED. ERROR.");
+            return 0;
+        }
+        const char *actualName = path;
+        if (oDescr->filterType & 2){
+            actualName += baseNameIndex;
+        }
+
+        if (oDescr->filterType & 1){
+            if (!strcmp(actualName, oDescr->filename)){
+                return 0;
+            }
+        }
+        else{
+            if (!strncmp(actualName, oDescr->filename, strlen(oDescr->filename))){
+                return 0;
+            }
+        }
+    }
+
     return 1;
 }
